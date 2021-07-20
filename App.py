4437e5d956c481ab9ae2e78a1d4b673a14cc8c0e
thiagopaes93 <thiagopaes1993@gmail.com>
2021-07-20 11:02:25 -0300

    first commit

---
+++ App.py
@@ -0,0 +1,121 @@
+from re import X
+from dash import Dash
+from dash_html_components import H1, Div, P
+from dash_core_components import Graph, DatePickerSingle, Checklist
+from dash.dependencies import Input, Output
+from pandas.core.tools.numeric import to_numeric
+import scrape
+import datas
+import pandas as pd
+import plotly.express as px
+
+# Variáveis iniciais
+stations = ['IMACA7', 'IMACA13', 'ICAMPO96', 'IMACA15']
+
+cidade = 'Macaé, RJ'
+
+
+df = pd.read_csv('data.csv', index_col=0)
+df.columns = scrape.colunas_total
+# Confere se o dia anterior foi computado
+
+filtered_df = df[df['station'] == 'IMACA7']
+tempdf = filtered_df[['t high', 't low']]
+tempdf = tempdf.apply(pd.to_numeric)
+fig = px.bar(tempdf)
+
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- DASH
+# Define o objeto Dash
+app = Dash(__name__)
+
+# Layout (como a página em si será apresentada)
+app.layout = Div(
+    className="app-header",
+    children=[
+        H1(cidade),
+
+        Div(
+            className='history',
+            children=[
+                datas.ontem
+            ]
+        ),
+
+        #DatePickerSingle(
+        #    display_format='DD/MM/YYYY',
+        #    id='my-date-picker-single',
+        #    date=datas.ontem
+        #),
+
+        Checklist(
+            id='checklist',
+            options=[
+                {'label': 'IMACA7', 'value': 'IMACA7'},
+                {'label': 'IMACA13', 'value': 'IMACA13'},
+                {'label': 'ICAMPO96', 'value': 'ICAMPO96'},
+                {'label': 'IMACA15', 'value': 'IMACA15'}
+            ],
+            value=['IMACA7', 'IMACA13','ICAMPO96','IMACA15']
+        ),
+
+        Div(
+            className='graphs',
+            children=[
+                Graph(id='grafico1'),
+                Graph(id='grafico2'),
+                Graph(id='grafico3'),
+                Graph(id='grafico4')
+            ]
+        )
+    ]
+)
+
+# Pega a variável que foi inserida na data
+@app.callback(
+    Output('grafico1', 'figure'),
+    Output('grafico2', 'figure'),
+    Output('grafico3', 'figure'),
+    Output('grafico4', 'figure'),
+    Input('checklist', 'value'))
+def update_output(station):
+    # Filtra pela estação
+    filtered_df = df[df.station.isin(station)]
+    # Filtra pela data
+    filtered_df2 = filtered_df[filtered_df['date']==datas.ontem]
+    # Pega as colunas de temperatura para o gráfico 1
+    tempdf = filtered_df2[['t high', 't low']]
+    tempdf = tempdf.apply(pd.to_numeric)
+    fig1 = px.bar(tempdf, barmode='group', template="ygridoff",color_discrete_sequence=['red','blue'])
+    fig1.update_layout(title='Temperatura',yaxis_title="Temperatura (°F)",
+    xaxis_title="Estação",legend_title="Legenda", plot_bgcolor="#222222", paper_bgcolor='#222222',
+    font_color="#e6e3e3",xaxis_tickfont_size=1)
+
+    # Pega as colunas de humidade relativa para o gráfico 2
+    rhdf = filtered_df2[['rh high', 'rh low']]
+    rhdf = rhdf.apply(pd.to_numeric)
+    fig2 = px.bar(rhdf, barmode='group', template="ygridoff",color_discrete_sequence=['red','blue'])
+    fig2.update_layout(title='Humidade Relativa',yaxis_title="Humidade (%)",
+    xaxis_title="Estação",legend_title="Legenda", plot_bgcolor="#222222", paper_bgcolor='#222222',
+    font_color="#e6e3e3",xaxis_tickfont_size=1)
+
+    # Pega as colunas de velocidade do vento para o gráfico 3
+    winddf = filtered_df2[['wind high', 'gust high']]
+    winddf = winddf.apply(pd.to_numeric)
+    fig3 = px.bar(winddf, barmode='group', template="ygridoff",color_discrete_sequence=['red','blue'])
+    fig3.update_layout(title='Velocidade do vento',yaxis_title="Velocidade (mph)",
+    xaxis_title="Estação",legend_title="Legenda", plot_bgcolor="#222222", paper_bgcolor='#222222',
+    font_color="#e6e3e3",xaxis_tickfont_size=1)
+
+    # Pega a coluna de precipitação para o gráfico 4
+    precdf = filtered_df2[['prec']]
+    precdf = precdf.apply(pd.to_numeric)
+    fig4 = px.bar(precdf, template="ygridoff",color_discrete_sequence=['blue'])
+    fig4.update_layout(title='Precipitação',yaxis_title="Precipitação (mm)",
+    xaxis_title="Estação",legend_title="Legenda", plot_bgcolor="#222222", paper_bgcolor='#222222',
+    font_color="#e6e3e3",xaxis_tickfont_size=1)
+    return fig1, fig2, fig3, fig4
+
+# Roda o servidor interno (8050)
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ datas.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta, timezone
+from dateutil.relativedelta import relativedelta
+
+# Fuso horário
+timezone_offset = -3.0
+tzinfo = timezone(timedelta(hours = timezone_offset))
+
+# Formatação da data no link do site
+ontem = (datetime.now(tzinfo) - relativedelta(days=1)).strftime("%Y-%m-%d")
+
+# Input numero de dias passados e retorna a data no formato do link
+def dia_atras_tabela (dias_atras):
+    dia = (datetime.now()-relativedelta(days=dias_atras)).strftime("%Y-%m-%d")
+    return dia
+
+
+
+
+# ------------- Formatação da data na tabela do site
+# Ontem no formato usado na tabela:
+ontem_tabela = (datetime.now(tzinfo) - relativedelta(days=1)).strftime("%m/%d/%Y")
+# Tira o zero do início se o mês menor que 10
+if int((datetime.now(tzinfo) - relativedelta(days=1)).strftime("%m")) < 10:
+    ontem_tabela = ontem_tabela[1:]
+
+# Hoje no formato da tabela
+if int(datetime.now(tzinfo).strftime('%m')) < 10:
+    hoje_tabela = datetime.now(tzinfo).strftime("%m/%d/%Y")[1:]
+else:
+    hoje_tabela = datetime.now(tzinfo).strftime("%m/%d/%Y")
