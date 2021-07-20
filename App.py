40ad77f3a7ef0a19bb9f2f78f9e7c49af96bfbf1
thiagopaes93 <thiagopaes1993@gmail.com>
2021-07-20 13:01:24 -0300

    graph layout updates

--- App.py
+++ App.py
@@ -21,7 +21,6 @@ if datas.ontem not in df.values:
         data = scrape.scrape_daily_day(station, datas.ontem)
         data.to_csv('data.csv', mode='a', header=False)
 
-
 # Lê as informações atualizadas
 df = pd.read_csv('data.csv', index_col=0)
 df.columns = scrape.colunas_total
@@ -92,38 +91,57 @@ def update_output(station):
     # Filtra pela data
     filtered_df2 = filtered_df[filtered_df['date']==datas.ontem]
     # Pega as colunas de temperatura para o gráfico 1
-    tempdf = filtered_df2[['t high', 't low']]
-    tempdf = tempdf.apply(pd.to_numeric)
-    fig1 = px.bar(tempdf, barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
+    tempdf = filtered_df2[['t high', 't low','station']]
+    fig1 = px.bar(tempdf,x='station',y=['t high', 't low'], barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
     fig1.update_layout(title='Temperatura',yaxis_title="Temperatura (°F)",
     xaxis_title="Estação",legend_title="Legenda", plot_bgcolor=bg_color, paper_bgcolor=bg_color,
-    font_color=font_color,xaxis_tickfont_size=1)
+    font_color=font_color,xaxis_tickfont_size=12, showlegend=False)
+    # Mostra os valores em cima do gráfico
+    texts1 = [tempdf['t high'], tempdf['t low']]
+    for i, t in enumerate(texts1):
+        fig1.data[i].text = t
+        fig1.data[i].textposition = 'inside'
+
+
 
     # Pega as colunas de humidade relativa para o gráfico 2
-    rhdf = filtered_df2[['rh high', 'rh low']]
-    rhdf = rhdf.apply(pd.to_numeric)
-    fig2 = px.bar(rhdf, barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
+    rhdf = filtered_df2[['rh high', 'rh low','station']]
+    fig2 = px.bar(rhdf, x='station', y=['rh high', 'rh low'], barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
     fig2.update_layout(title='Humidade Relativa',yaxis_title="Humidade (%)",
     xaxis_title="Estação",legend_title="Legenda", plot_bgcolor=bg_color, paper_bgcolor=bg_color,
-    font_color=font_color,xaxis_tickfont_size=1)
+    font_color=font_color,xaxis_tickfont_size=12, showlegend=False)
+    # Mostra os valores em cima do gráfico
+    texts2 = [rhdf['rh high'], rhdf['rh low']]
+    for i, t in enumerate(texts2):
+        fig2.data[i].text = t
+        fig2.data[i].textposition = 'inside'
 
     # Pega as colunas de velocidade do vento para o gráfico 3
-    winddf = filtered_df2[['wind high', 'gust high']]
-    winddf = winddf.apply(pd.to_numeric)
-    fig3 = px.bar(winddf, barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
+    winddf = filtered_df2[['gust high', 'wind high','station']]
+    fig3 = px.bar(winddf, x= 'station', y= ['gust high', 'wind high'], barmode='group', template="ygridoff",color_discrete_sequence= bar_colors)
     fig3.update_layout(title='Velocidade do vento',yaxis_title="Velocidade (mph)",
     xaxis_title="Estação",legend_title="Legenda", plot_bgcolor=bg_color, paper_bgcolor=bg_color,
-    font_color=font_color,xaxis_tickfont_size=1)
+    font_color=font_color,xaxis_tickfont_size=12, showlegend=False)
+    # Mostra os valores em cima do gráfico
+    texts3 = [winddf['gust high'], winddf['wind high']]
+    for i, t in enumerate(texts3):
+        fig3.data[i].text = t
+        fig3.data[i].textposition = 'inside'
 
     # Pega a coluna de precipitação para o gráfico 4
-    precdf = filtered_df2[['prec']]
-    precdf = precdf.apply(pd.to_numeric)
-    fig4 = px.bar(precdf, template="ygridoff",color_discrete_sequence= bar_colors)
-    fig4.update_layout(title='Precipitação',yaxis_title="Precipitação (mm)",
+    precdf = filtered_df2[['prec','station']]
+    fig4 = px.bar(precdf,x= 'station', y='prec', template="ygridoff",color_discrete_sequence= ['#0091D5'])
+    fig4.update_layout(title='Precipitação',yaxis_title="Precipitação (in)",
     xaxis_title="Estação",legend_title="Legenda", plot_bgcolor=bg_color, paper_bgcolor=bg_color,
-    font_color=font_color,xaxis_tickfont_size=1)
-    return fig1, fig2, fig3, fig4
+    font_color=font_color,xaxis_tickfont_size=12, showlegend=False)
+    # Mostra os valores em cima do gráfico
+    texts4 = [precdf['prec']]
+    for i, t in enumerate(texts4):
+        fig4.data[i].text = t
+        fig4.data[i].textposition = 'inside'
 
+    # Retorna os gráficos
+    return fig1, fig2, fig3, fig4
 # Roda o servidor interno (8050)
 if __name__ == '__main__':
     app.run_server(debug=True)
